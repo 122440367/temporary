bb3467934137e9c88df1e4a4573e06353164c1bf
AK12_Official <995929122@qq.com>
2025-04-01 18:43:55 +0800

    自封装的一个Svg图标组件

---
+++ project/src/components/SvgIcon/index.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <svg :style="{ width: width, height: height }">
+        <use :href="prefix + name" :fill="color" :width="width" :height="height"></use>
+    </svg>
+
+</template>
+
+
+<script setup lang="ts">
+//接收父组件参数
+defineProps({
+    //属性值前缀
+    prefix: {
+        type: String,
+        default: '#icon-'
+    },
+    //图标名称
+    name: {
+        type: String,
+    },
+    //图标颜色
+    color: {
+        type: String,
+        default: ''
+    },
+    //图标大小
+    width: {
+        type: String,
+        default: '16px'
+    },
+    height: {
+        type: String,
+        default: '16px'
+    }
+})
+
+</script>
+
+<style scoped></style>
