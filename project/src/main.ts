3bc10aa0f63fc41e37e31a38c140fcd7a488e0a9
AK12_Official <995929122@qq.com>
2025-04-03 00:47:39 +0800

    重新格式化

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,12 +1,15 @@
 
 import '@/style/index.scss' //引入全局样式
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'         //引入ElementPlus及样式
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'        //配置element-plus国际化
-import zhCn from 'element-plus/es/locale/lang/zh-cn' //引入中文语言包
+import { createPinia } from 'pinia'         
+import ElementPlus from 'element-plus'      
+import 'element-plus/dist/index.css'        //引入ElementPlus及样式
+import zhCn from 'element-plus/es/locale/lang/zh-cn' //配置国际化 引入中文语言包
 import App from './App.vue'
-
+import 'virtual:svg-icons-register' //引入svg图标注册
+//引入自定义插件对象：注册整个项目全局组件
+import globalComponent from '@/components'
+import router from '@/router' //引入路由器对象
 
 
 const app = createApp(App)
@@ -19,11 +22,8 @@ app.use(ElementPlus, {
 })
 
 
+app.use(router) //安装路由器对象
 
-import 'virtual:svg-icons-register' //引入svg图标注册
-
-//引入自定义插件对象：注册整个项目全局组件
-import globalComponent from '@/components'
 //安装自定义插件
 app.use(globalComponent)
 
