c87619b202ce4f26b4c66bddb477d7d88e59dee9
AK12_Official <995929122@qq.com>
2025-04-06 00:53:42 +0800

    简单的逻辑空壳

--- project/src/views/home/index.vue
+++ project/src/views/home/index.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
         <h1>我是一级路由展示登陆成功后的数据 </h1>
-        
+
     </div>
 </template>
 
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { GET_TOKEN } from '@/utils/token';
 
+let $router = useRouter();
+
+const redirect = () => {
+    // 获取 token
+    const token = GET_TOKEN();
+    console.log(token);
+    // 如果没有 token，跳转到登录页面
+    if (!token) {
+        $router.push('/login'); // 使用 Vue Router 的编程式导航
+    }
+};
+
+// 调用函数
+redirect();
 </script>
 
-<style scoped >
-
-</style>
+<style scoped></style>
