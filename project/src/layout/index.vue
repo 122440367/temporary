d5e86149530e74c4fb1d7b8e5b14b8dcb7d7f578
AK12_Official <995929122@qq.com>
2025-04-06 01:48:48 +0800

    layout静态搭建

---
+++ project/src/layout/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="layout_container">
+        <!-- 左侧菜单 -->
+        <div class="layout_sidebar">
+            <Logo></Logo>
+        </div>
+        <!-- 顶部tabbar -->
+        <div class="layout_tabbar">456</div>
+        <!-- 主要内容区 -->
+        <div class="layout_main">
+            <p class="test">
+
+
+            </p>
+        </div>
+
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { GET_TOKEN } from '@/utils/token';
+import Logo from './logo/index.vue';
+
+let $router = useRouter();
+
+const redirect = () => {
+    const token = GET_TOKEN();
+    if (!token) {
+        $router.push('/login');
+    }
+};
+
+// 调用函数
+redirect();
+</script>
+
+<style scoped lang="scss">
+.layout_container {
+    width: 100%;
+    height: 100vh;
+    // position: relative;
+    .layout_sidebar {
+        width: $base-menu-width;
+        height: 100%;
+        background: $base-menu-background;
+    }
+
+    .layout_tabbar {
+        position: fixed;
+        width: calc(100% - $base-menu-width);
+        height: $base-tabbar-height;
+        top: 0px;
+        left: $base-menu-width;
+    }
+
+    .layout_main {
+        position: absolute;
+        width: calc(100% - $base-menu-width);
+        height: calc(100vh - $base-tabbar-height);
+        left: $base-menu-width;
+        top: $base-tabbar-height;
+        padding: 20px;
+        overflow: auto;
+    }
+
+    .test {
+        height: 10000px;
+    }
+}
+</style>
---
+++ project/src/layout/logo/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="logo">
+        <img :src="setting.logo" alt="" v-if="!setting.logiHidden">
+        <p>{{ setting.title }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import setting from '@/setting';
+
+</script>
+
+
+<style scoped lang="scss">
+.logo {
+    width: 100%;
+    height: auto;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    img {
+        width: 120px;
+        // height: 120px;
+    }
+
+    p {
+        margin-left: 10px;
+        font-size: $base-logo-title-fontsize;
+    }
+}
+</style>
