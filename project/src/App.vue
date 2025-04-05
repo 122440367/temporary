3be64e7b3b9ac8febcad9240b828ea7b67a5e513
AK12_Official <995929122@qq.com>
2025-04-05 20:12:03 +0800

    静态资源和环境变量宏的调整

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,31 +1,16 @@
 <template>
   <div>
     <router-view></router-view>
-    <svg-icon name="test" color="#f00" width="50px" height="50px"></svg-icon>
   </div>
 
 </template>
 
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { reqLogin } from './api/user';
-import router from './router';
 
-onMounted(() => {
-  // console.log('App组件挂载完成');
-  reqLogin({
-    username: 'admin',
-    password: '123456'
-  })
-});
 
 </script>
 
 <style scoped lang="scss">
-div {
-  h1 {
-    color: $base-color;
-  }
-}
+
 </style>
