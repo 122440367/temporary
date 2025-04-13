36bb2219a9f1445c3f6b01ffe7e7d15bfdd9f12d
AK12_Official <995929122@qq.com>
2025-04-13 11:48:31 +0800

    属性管理静态搭建

--- project/src/components/index.ts
+++ project/src/components/index.ts
@@ -1,10 +1,11 @@
 //引入项目中全部的全局组件
 import SvgIcon from './SvgIcon/index.vue' //引入svg组件
 import * as ElIcon from '@element-plus/icons-vue' //引入element-plus的图标组件
-
+import Category from './Category/index.vue' //引入分类组件
 //全局对象
 const allGlobalcomponents = {
     SvgIcon,
+    Category
 }
 
 
--- project/src/views/product/attr/index.vue
+++ project/src/views/product/attr/index.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>属性管理</h1>
+    <div class="root">
+        
+        <Category></Category>
+        
+        <el-card style="margin:10px 0">
+            <el-button type="primary" size="large" icon="Plus" style="margin: 10px 0;">添加属性</el-button>
+            <el-table border>
+                <el-table-column label="序号" align="center" type="index" width="120px"></el-table-column>
+                <el-table-column label="属性名称" width="280px"  align="center"></el-table-column>
+                <el-table-column label="属性值"  align="center"></el-table-column>
+                <el-table-column label="操作" width="220px"  align="center"></el-table-column>
+            </el-table>
+        </el-card>
+    </div>
 </template>
 <script setup lang="ts">
 
 </script>
 
-<style>
-
-</style>
+<style></style>
